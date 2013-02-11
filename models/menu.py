76c9785e6659df602320761d01abcb398ddd17ff
ilvalle <paolo.valleri@gmail.com>
2013-02-11 14:52:47 +0100

    added language files and social_buttons

--- models/menu.py
+++ models/menu.py
@@ -4,5 +4,5 @@ response.meta.author = '%(author)s <%(author_email)s>' % settings
 response.meta.keywords = settings.keywords
 response.meta.description = settings.description
 response.menu = [
-(T('Index'),URL('default','index')==URL(),URL('default','index'),[]),
-]
+	(A('INTEGREEN', _href="http://integreen-life.bz.it/", _class="brand"), False, None),
+	(T('Index'),URL('default','index')==URL(),URL('default','index'),[]),]
