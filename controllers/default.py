14274b45974ee0b978c20ed0d45e4976c5f6f502
ilvalle <paolo.valleri@gmail.com>
2013-02-14 18:50:23 +0100

    initial import to show history data

--- controllers/default.py
+++ controllers/default.py
@@ -1,19 +1,35 @@
 # -*- coding: utf-8 -*-
 from xmlrpclib import ServerProxy
 server = ServerProxy("http://ipchannels-frontend.integreen-life.bz.it/xmlRpcFrontEnd/xmlrpc")
+from datetime import datetime
 
 def user(): return dict(form=auth())
 
 def index():
-	parks_id = cache.ram('parkingIds', lambda: server.DataManager.getParkingIds(), time_expire=3600)
-	if parks_id == -1:
-		return 'errore'
-	parks = []
-	for park in parks_id:
-		data = __get_park_data(park)
-		parks.append( data )
-	return {'parks': parks}
+	try:
+		parks = __get_parks_info()
+		return {'parks': parks}
+	except Exception:
+		return 'Data not available'
+def plot():
+	park_id = request.args(0) or 'index'
+	if not(park_id and park_id.isdigit()): raise HTTP(404)
+	parks = __get_parks_info()
+	return {'parks': parks, 'park_id':park_id}
 
+def get_history():
+	if not(request.ajax): raise HTTP(403)
+	park_id = request.args(0) or 'index'
+	interval = request.vars.interval or 'index'
+	if not(park_id and park_id.isdigit()): raise HTTP(404)
+	data = server.DataManager.getFreeSlotsByTimeAndParkingArea(int(park_id), 24*60*7)
+	output = []
+	for element in data:
+		epoch = (datetime.strptime(str(element[1]), "%Y%m%dT%H:%M:%S") - datetime(1970,1,1)).total_seconds() 
+		milli_epoch = epoch * 1000
+		output.append([milli_epoch,element[0]])
+	return response.render('generic.json', {'park_%s' % park_id:{'data':output}})	
+	
 def freeslots():
 	if not(request.ajax): raise HTTP(403)
 	park_id = request.args(0) or 'index'
@@ -38,3 +54,14 @@ def __get_park_data(park):
 	data['slots_taken_rate'] = (data['slots_taken'] * 100) / data['slots']
 	data['freeslots_rate'] = 100 - data['slots_taken_rate']
 	return data
+
+def __get_parks_info():
+	parks_id = cache.ram('parkingIds', lambda: server.DataManager.getParkingIds(), time_expire=3600)
+	if parks_id == -1:
+		return 'errore'
+	parks = []
+	for park in parks_id:
+		data = __get_park_data(park)
+		parks.append( data )
+	return parks
+
