2cd3b9c685fbe11cc6b4c4f83d2f02f9158cde01
ilvalle <paolo.valleri@gmail.com>
2013-02-17 19:32:54 +0100

    several css/js fix

--- controllers/default.py
+++ controllers/default.py
@@ -21,15 +21,15 @@ def trend():
 def get_history():
 	if not(request.ajax): raise HTTP(403)
 	park_id = request.args(0) or 'index'
-	interval = request.vars.interval or 'index'
+	n_days = int(request.vars.interval) if request.vars.interval and request.vars.interval.isdigit() else 7
 	if not(park_id and park_id.isdigit()): raise HTTP(404)
-	data = server.DataManager.getFreeSlotsByTimeAndParkingArea(int(park_id), 24*60*7)
+	data = server.DataManager.getFreeSlotsByTimeAndParkingArea(int(park_id), 24*60*n_days)
 	output = []
 	for element in data:
 		epoch = (datetime.strptime(str(element[1]), "%Y%m%dT%H:%M:%S") - datetime(1970,1,1)).total_seconds() 
 		milli_epoch = epoch * 1000
 		output.append([milli_epoch,element[0]])
-	return response.render('generic.json', {'park_%s' % park_id:{'data':output}})	
+	return response.render('generic.json', {'park_%s' % park_id:{'data':output, 'park_id':park_id}})	
 	
 def freeslots():
 	if not(request.ajax): raise HTTP(403)
