9c6653738c2d747390d23f05247c783d4d237ac3
ilvalle <paolo.valleri@gmail.com>
2013-02-18 13:37:52 +0100

    initial ie support

--- models/menu.py
+++ models/menu.py
@@ -3,6 +3,7 @@ response.subtitle = settings.subtitle
 response.meta.author = '%(author)s <%(author_email)s>' % settings
 response.meta.keywords = settings.keywords
 response.meta.description = settings.description
+response.page_title=T("Bolzano parking situation")
 response.menu = [
 	(A('INTEGREEN', _href="http://integreen-life.bz.it/", _class="brand"), False, None),
 	(T('Index'),URL('default','index')==URL(),URL('default','index'),[]),]
