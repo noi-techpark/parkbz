dae53fa5f2fe929b87e11d1c3286abac51dd1408
ilvalle <paolo.valleri@gmail.com>
2014-06-13 12:44:20 +0200

    more css fix

--- static/js/utility.js
+++ static/js/utility.js
@@ -1,28 +1,28 @@
-$(document).ready(function() {
-		
-	
+function initTooltip(){
 	// Remove TEL link on desktop
     var width = $(window).width();
     if( width >= 768) {
         $('.phone a').each(function(){
             var tel = $(this).html();
             $(this).replaceWith('<span>'+tel+'</span>');
-            
         });
-    } 
+    }
+
+	// Handle graph
+    $('.forecast .graph').hide();
+	$('.forecast h3').click(function(){
+		$(this).toggleClass('open').next().slideToggle('fast');
+	})
 	
+};
+$(document).ready(function() {
 	
 	$('.actions .time span,.carpark-selector span').click(function(){
 		$(this).toggleClass('open').next().fadeToggle('fast');
 	})
 	
-	$('.forecast .graph').hide();
-	
-	$('.forecast h3').click(function(){
-		$(this).toggleClass('open').next().slideToggle('fast');
-	})
-	
 	$('.widget').hide();
+
 	$('.buttons .share-btn').click(function(){
 		$(this).toggleClass('open');
 		$('.widget').slideToggle('fast');
@@ -30,12 +30,11 @@ $(document).ready(function() {
 		return false;
 	})
 	
+	initTooltip();
 	
 	
 	// ---- Link esterno ----------------------------------------------------------------------------------------------------------
 	
 	$("a[href*='http://']:not([href*='"+location.hostname+"']),[href*='https://']:not([href*='"+location.hostname+"'])").attr("target","_blank");	
-	
-	
 		
 });	
