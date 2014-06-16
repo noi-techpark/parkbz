1bffe6b8e42e1f2577518657418c6f6983904326
ilvalle <paolo.valleri@gmail.com>
2014-06-16 17:21:44 +0200

    fix other css issues

--- static/js/utility.js
+++ static/js/utility.js
@@ -1,25 +1,37 @@
 function initTooltip(){
-	// Remove TEL link on desktop
-    var width = $(window).width();
-    if( width >= 768) {
-        $('.phone a').each(function(){
-            var tel = $(this).html();
-            $(this).replaceWith('<span>'+tel+'</span>');
-        });
-    }
+		// Remove TEL link on desktop
+	    var width = $(window).width();
+	    if( width >= 768) {
+	        $('.phone a').each(function(){
+	            var tel = $(this).html();
+	            $(this).replaceWith('<span>'+tel+'</span>');
+	        });
+	    }
 
-	// Handle graph
-    $('.forecast .graph').hide();
-	$('.forecast h3').click(function(){
-		$(this).toggleClass('open').next().slideToggle('fast');
-	})
-	
-};
+		// Handle graph
+	    $('.forecast .graph').hide();
+		$('.forecast h3').click(function(){
+			$(this).toggleClass('open').next().slideToggle('fast');
+		})
+		
+	};
 $(document).ready(function() {
 	
 	$('.actions .time span,.carpark-selector span').click(function(){
+		if(!$(this).hasClass('open')){
+			var docHeight = $(document).height();
+			$('<div class="overlay-dropdown" style="width:100%;height:'+docHeight+'px;position:absolute;top:0;left:0;bottom:0;right:0;z-index:120"></div>').appendTo('body');
+			overlayDropdown(this);
+		} else {$('.overlay-dropdown').remove();}
 		$(this).toggleClass('open').next().fadeToggle('fast');
 	})
+	
+	function overlayDropdown (e){
+		$('.overlay-dropdown').click(function(){
+			e.click();
+			$('.overlay-dropdown').remove();
+		});		
+	};	
 	
 	$('.widget').hide();
 
